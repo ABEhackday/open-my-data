cf434a9ef0d098e37be9fefabe5217b7b0b388f7
Albert Song <albb0920@gmail.com>
2013-11-02 18:34:31 +0800

    drop coffee script.

--- app/assets/javascripts/datasets.js
+++ app/assets/javascripts/datasets.js
@@ -1,21 +1,30 @@
 $(function () {
-    if ($('body').hasClass('new-action') && $('body').hasClass('datasets-controller')) {   
-        var wrappers = $('.wrapper');
-        $('button.next').click(function () {
-            wrappers.eq(0).animate({
-                left: "-100%"
-            }, 450);
-            wrappers.eq(1).animate({
-                left: "0"
-            }, 450);
-        });
-        $('button.prev').click(function () {
-            wrappers.eq(0).animate({
-                left: "0"
-            }, 450);
-            wrappers.eq(1).animate({
-                left: "100%"
-            }, 450);
-        });
+    if(!$('body').hasClass('datasets-controller'))
+	return;
+
+    if ($('body').hasClass('new-action') {
+	var wrappers = $('.wrapper');
+	$('button.next').click(function () {
+	    wrappers.eq(0).animate({
+		left: "-100%"
+	    }, 450);
+	    wrappers.eq(1).animate({
+		left: "0"
+	    }, 450);
+	});
+	$('button.prev').click(function () {
+	    wrappers.eq(0).animate({
+		left: "0"
+	    }, 450);
+	    wrappers.eq(1).animate({
+		left: "100%"
+	    }, 450);
+	});
+    }else if($('body').hasClass('show')){
+	$('table.dataset-preview').dataTable({
+	    bJQueryUI: true
+	});
+    }else if($('body').hasClass('index'){
+    
     }
 });
--- app/assets/javascripts/datasets.js.coffee
+++
@@ -1,6 +0,0 @@
-$ ->
-  if !$('body').hasClass('datasets-controller show-action')
-    return
-
-  $('table.dataset-preview').dataTable
-    bJQueryUI: true
