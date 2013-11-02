bd0314f1089a5ed00a2fd8f70fa872945448e360
Albert Song <albb0920@gmail.com>
2013-11-02 20:09:23 +0800

    gogo more detail message

--- app/assets/javascripts/datasets.js
+++ app/assets/javascripts/datasets.js
@@ -3,26 +3,46 @@ $(function () {
 	return;
 
     if ($('body').hasClass('new-action')) {
-	var wrappers = $('.wrapper');
-	$('button.next').click(function () {
-	    wrappers.eq(0).animate({
-		left: "-100%"
-	    }, 450);
-	    wrappers.eq(1).animate({
-		left: "0"
-	    }, 450);
-	});
-	$('button.prev').click(function () {
-	    wrappers.eq(0).animate({
-		left: "0"
-	    }, 450);
-	    wrappers.eq(1).animate({
-		left: "100%"
-	    }, 450);
-	});
-    }else if($('body').hasClass('show-action')){
-	$('table.dataset-preview').dataTable({ bJQueryUI: true });
-    }else if($('body').hasClass('index-action')){
-    
+    	var wrappers = $('.wrapper');
+    	$('button.next').click(function () {
+    	    wrappers.eq(0).animate({
+    		left: "-100%"
+    	    }, 450);
+    	    wrappers.eq(1).animate({
+    		left: "0"
+    	    }, 450);
+    	});
+    	$('button.prev').click(function () {
+    	    wrappers.eq(0).animate({
+    		left: "0"
+    	    }, 450);
+    	    wrappers.eq(1).animate({
+    		left: "100%"
+    	    }, 450);
+    	});
+    } else if ($('body').hasClass('show-action')) {
+	    $('table.dataset-preview').dataTable({ bJQueryUI: true, bAutoWidth: true });
+    } else if ($('body').hasClass('index-action')) {
+        var template = Handlebars.compile($("#t-datasets-more").html());
+        $('.dataset').click(function () {
+            var id = $(this).attr('data-id'),
+                more = $(this).find('.more'),
+                active = $(this).data('active'),
+                self = this;
+            if (!active) {
+                $.ajax({
+                    url: '/datasets/' + id + '.json',
+                    format: 'json',
+                    success: function (data) {
+                        more.html(template(data));
+                        more.slideDown();
+                        $(self).data('active', true);
+                    }
+                });
+            } else {
+                more.slideUp();
+                $(this).data('active', false);
+            }
+        });
     }
 });
