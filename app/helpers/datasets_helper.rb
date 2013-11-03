b5dd10f105456df2272fb33698513a76a3196b67
Albert Song <albb0920@gmail.com>
2013-11-03 12:30:34 +0800

    refince style.

--- app/helpers/datasets_helper.rb
+++ app/helpers/datasets_helper.rb
@@ -1,2 +1,11 @@
 module DatasetsHelper
+  def score_color(score)
+    if score == nil || score == 3.0
+      return "dimgray"
+    elsif score > 3.0
+      return "forestgreen"
+    else
+      return "crimson"
+    end
+  end
 end
